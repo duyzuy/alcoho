47e47777f3c120141659e1bd03cfb34e51d019c9
vu truong duy <vutruongduy2109@gmail.com>
2023-07-08 00:14:05 +0700

    update

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,14 +6,7 @@ $(".blog-list").slick({
   arrows: false,
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-
-    {
-      breakpoint: 994,
+      breakpoint: 992,
       settings: {
         slidesToShow: 2,
       },
@@ -36,4 +29,47 @@ $(".top-slider").slick({
     "<span class='slick-arrow prev'><i class='fa-solid fa-angle-left'></i></span>",
   nextArrow:
     "<span class='slick-arrow next'><i class='fa-solid fa-angle-right'></i></span>",
+});
+
+$("#product-slide").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+$("#testimonial-slide").slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 468,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
